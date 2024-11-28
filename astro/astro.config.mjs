74cd42e04a5c69476e0188652a50e480b376696c
Frederik Hørgreen <frederik@hoergreen.dk>
2024-11-28 12:29:25 +0100

    add sitemap

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import {defineConfig} from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  })],
-  build: {
-    assets: '_assets'
-  },
-  trailingSlash: 'never'
+    site: 'https://www.hrgn.dk',
+    integrations:  [
+        sitemap(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+    ],
+    build:         {
+        assets: '_assets',
+    },
+    trailingSlash: 'never',
 });
